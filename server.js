520966c063be493793fcbc2c40ec4a5475398381
fiir09 <114342288+fiir09@users.noreply.github.com>
2022-11-20 21:24:55 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import userRoute from './routes/userRoute.js';
+
+dotenv.config();
+connectDB()
+const app = express()
+
+if(process.env.MODE === 'development'){
+    app.use(morgan('dev'))
+}
+
+app.use(express.json());
+app.use('/api', userRoute)
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on ${PORT}`))
