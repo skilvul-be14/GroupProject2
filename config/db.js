99a1b1f84c5e5a77081a6b9dbbdb85e8b7552609
fiir09 <114342288+fiir09@users.noreply.github.com>
2022-11-21 09:01:17 +0700

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        })
+        console.log(`MongoDB is connected : ${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+        console.log(`MongoDB Error : ${error.message}`.red.underline)
+        process.exit(1)
+    }
+}
+
+export default connectDB
